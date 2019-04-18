967ea6a6563fc3245be2bf0439743817c400b4c4
Nicole Speirs <nickispeirs@gmail.com>
2019-04-18 17:43:33 -0500

    intro to data structures: complete -2

--- intro-data-structures.js
+++ intro-data-structures.js
@@ -1,7 +1,35 @@
 var members = ["Vivian", "Ava", "Josh", "Patrick", "Mike"];
 
-function addToLine(name){
+
+function addPerson(name) {
   members.push(name);
 }
 
-console.table(addToLine("Mary"));
+function findPerson(name) {
+  results = members.indexOf(name);
+  if (results === -1) {
+    return "Sorry " + name + " is not in line!";
+  } else return name + "'s" + " number in line is " + results + "!";
+}
+
+function skipLine(num, name) {
+  var remove = 0;
+  members.splice(num, remove, name);
+  return members.join(", ");
+}
+
+function leaveLine(name) {
+  let arr = members;
+  for (var i = 0; i <= arr.length; i++) {
+    if (arr[i] === name) {
+      arr.splice(i, 1);
+      return members.join(", ");
+    }
+  }
+}
+console.log(addPerson("Mary"));
+console.table(members);
+console.log(findPerson("Ava"));
+console.log(findPerson("Bee"));
+console.log(skipLine(2, "Zoe"));
+console.log(leaveLine("Mike"));
